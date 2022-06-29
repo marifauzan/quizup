ed41c9ff43b97f3da0aeccf186083e40d8370c2e
gungyusma <61109894+gungyusma@users.noreply.github.com>
2022-06-29 15:05:33 +0800

    fix warnings eslint

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+
+import React, { useState } from "react";
 
 export default function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(1);
